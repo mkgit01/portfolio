10da97722729ea2032c0cf7859f270b31b8b0328
MK Nayak <84683862+mkgit01@users.noreply.github.com>
2022-06-09 21:15:51 +0530

    fixed nav links of index

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,3 +3,20 @@ let loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
   loader.style.display = "none";
 })
+
+
+// JavaScript code
+function search_blog() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('blog-title');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="block";                 
+      }
+  }
+}
